892852f2c27b228244599a259fd296b400d706f6
Pahade <anpahade@ptc.com>
2019-02-11 14:57:01 +0530

    Changed few locator with id

--- OrangeHrm/src/test/java/ValidateSuccessfulLogin.java
+++ OrangeHrm/src/test/java/ValidateSuccessfulLogin.java
@@ -1,5 +1,7 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pages.DashboardPageObject;
 import util.BaseTestCase;
 
 public class ValidateSuccessfulLogin extends BaseTestCase {
@@ -7,7 +9,10 @@ public class ValidateSuccessfulLogin extends BaseTestCase {
 	@Test
 	public void Can_Successful_Login() {
 		openOrangeHrmWithDefault();
-		System.out.println("Successfuly logged in");
+		DashboardPageObject dashboard=new DashboardPageObject();
+		Assert.assertEquals(dashboard.getHeadLabelText(), "Dashboard");
+		
+		System.out.println("Successfuly verified dashboard page");
 	}
 
 }
