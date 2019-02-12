0bc4eeef6a1b1f2a91041fa280acf0e25240e5a8
Pahade <anpahade@ptc.com>
2019-02-12 16:59:16 +0530

    New submissions

---
+++ OrangeHrm/src/test/java/ValidateQuickLaunch.java
@@ -0,0 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.DashboardPageObject;
+import util.BaseTestCase;
+
+public class ValidateQuickLaunch extends BaseTestCase {
+
+	@Test
+	public void Can_Successful_Login() {
+		openOrangeHrmWithDefault();
+		DashboardPageObject dashboard=new DashboardPageObject();
+		Assert.assertEquals(dashboard.getHeadLabelText(), "Dashboard");
+		
+		System.out.println("Successfuly verified dashboard page");
+	}
+
+}
